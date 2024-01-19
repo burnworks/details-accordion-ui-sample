f6ff8a8288074f1c3c763a57e4c74dfa505b8beb
burnworks <6436833+burnworks@users.noreply.github.com>
2024-01-19 18:02:43 +0900

    add deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://burnworks.github.io',
   base: '/details-accordion-ui-sample',
   trailingSlash: "always",
-  outDir: './docs',
   integrations: [tailwind()]
 });
