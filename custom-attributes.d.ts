a71c4d93e2681df00c45a1fa7d68cfa13b82949b
burnworks <6436833+burnworks@users.noreply.github.com>
2024-01-20 13:05:21 +0900

    Update custom-attributes.d.ts

--- custom-attributes.d.ts
+++ custom-attributes.d.ts
@@ -1,6 +1,8 @@
 // details 要素に name 属性を付与できるように型定義
 declare namespace astroHTML.JSX {
-    interface HTMLAttributes {
-        'name'?: string;
+    interface IntrinsicElements {
+        details: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+            name?: string;
+        };
     }
 }
